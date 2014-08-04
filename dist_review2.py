c07bfbe0f67b18bb44ef4025b1e6697508a605f4
nyghtowl <warrick.melanie@gmail.com>
2014-08-03 19:59:09 -0700

    Worked on loading files and removing users with specific missing values.

---
+++ dist_review2.py
@@ -0,0 +1,31 @@
+'''
+Distribution Practice 2
+
+
+'''
+
+import matplotlib.pyplot as plt
+# import numpy as np
+# import scipy as sp
+import pandas as pd
+# from scipy import stats
+
+def read_file(filename):
+    return pd.read_csv(filename, dtype=object)
+
+def drop_marks(df):
+    # can pass in args columns, loop through and replace df accordingly
+    return df[(df.age != '?') & (df['max_heart_rate'] != '?') & (df['resting_blod_pressuremm_hg'] != '?') & (df['cholesterol_mg/dl'] != '?') & (df.sex != '?')]
+
+def main():
+    file_list = ['./data/cleveland_heart.csv','./data/hungarian_heart.csv', './data/switzerland_heart.csv', './data/long_beach.csv']
+
+    locations = []
+    
+    for filename in file_list:
+        location_name = filename.replace('.csv', '').replace('./', '')
+        location_df = drop_marks(read_file(filename))
+        locations.append(location_df)
+
+    return locations
+
